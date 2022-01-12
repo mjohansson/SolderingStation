868601c85e1b75959713c4328c0ffbda5729d8e2
MathiasJ <e96mjh@gmail.com>
2022-01-12 02:54:38 +0100

    Basic glcd convert-tool for the 4bpp OLED

--- tools/glcd_convert/glcd_convert.py
+++ tools/glcd_convert/glcd_convert.py
@@ -1,9 +1,8 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import os, sys
 import struct
 from PIL import Image
-
 
 def image_info(fn):
     try:
@@ -14,20 +13,36 @@ def image_info(fn):
         pass
 
 
-def image_convert(fn):
+def image_convert(fn, bpp=4):
     an = os.path.splitext(os.path.basename(fn))[0]
     try:
         im = Image.open(fn)
         print("Image " + os.path.basename(fn) + " opened...")
-        create_h_file(fn, an, im)
-        create_c_file(fn, an, im)
+        w, h = im.size
+        array_bytes = 0
+        if bpp == 1:
+            print("Creating 1bpp image data...")
+            an = an + "_1bpp"
+            array_bytes = ((w + 7) // 8) * h
+            s = image_to_1bpp_string(im)
+        elif bpp == 4:
+            print("Creating 4bpp image data...")
+            an = an + "_4bpp"
+            array_bytes = ((w + 1) // 2) * h
+            s = image_to_4bpp_string(im)
+        else:
+            print("ERROR, requested bits-per-pixels {} not supported!".format(bpp))
+
+        if len(s):
+            create_h_file(fn, an, im, array_bytes)
+            create_c_file(fn, an, im, s, array_bytes)
     except:
         print("ERROR, unable to open/convert image {}".format(fn))
     finally:
         im.close()
 
 
-def create_h_file(fn, array_name, image):
+def create_h_file(fn, array_name, image, array_bytes):
     f, e = os.path.splitext(fn)
     outfn = f + ".h"
     try:
@@ -54,7 +69,7 @@ def create_h_file(fn, array_name, image):
         of.write("#define {}_WIDTH ({})\n".format(array_name.upper(), imw + (imw%2) ))
         of.write("#define {}_HEIGHT ({})\n\n".format(array_name.upper(), imh))
 
-        of.write("extern const uint8_t {}[{}];\n\n".format(array_name, ((imw+1)/2)*imh))
+        of.write("extern const uint8_t {}[{}];\n\n".format(array_name, array_bytes) )
 
         of.write("#endif // __{}_H__\n\n".format(bn.upper()))
     except:
@@ -64,7 +79,7 @@ def create_h_file(fn, array_name, image):
         of.close()
 
 
-def create_c_file(fn, array_name, image):
+def create_c_file(fn, array_name, image, data_str, data_len):
     f, e = os.path.splitext(fn)
     outfn = f + ".c"
     try:
@@ -88,10 +103,10 @@ def create_c_file(fn, array_name, image):
         of.write("#include \"{}.h\"\n\n".format(bn))
 
         of.write("// Bitmap from \"" + fn + "\" ({}x{} pixels) : \n".format(imw, imh))
-        of.write("const uint8_t {}[{}] = {{\n".format(array_name, ((imw+1)/2)*imh))
+        of.write("const uint8_t {}[{}] = {{\n".format(array_name, data_len))
 
-        array_str = image_to_4bpp_string(image)
-        of.write(array_str)
+        #array_str = image_to_4bpp_string(image)
+        of.write(data_str)
 
         of.write("};\n\n")
     except:
@@ -122,7 +137,7 @@ def image_to_4bpp_string(image):
     lines = 0
     try:
         im = image_to_4bpp_image(image)
-#        im.show()
+        im.show()
         w, h = im.size
         for y in range(h):
             for x in range(0, w, 2):
@@ -145,12 +160,62 @@ def image_to_4bpp_string(image):
         return ""
 
 
+def image_to_1bpp_image(im_in):
+    try:
+        bw_image = im_in.convert("1")    # to 1b B/W
+        return bw_image
+    except:
+        print("ERROR, could not convert image to 1b b/w")
+        print(sys.exc_info())
+        return None
+
+
+def image_to_1bpp_string(image):
+    s = "    "
+    lines = 0
+    try:
+        im = image_to_1bpp_image(image)
+        im.show()
+        w, h = im.size
+        for y in range(h):
+            for x in range(0, w, 8):
+                p = 0
+                for b in range(8):
+                    v = 1 if x+b < w and im.getpixel((x+b,y)) else 0
+                    p = (p << 1) | v
+                s += "0x%02X, " % (p)
+                if x % 96 == 88:
+                    s += "\n    "
+                    lines += 1
+            if w % 96 != 0:
+                s += "\n    "
+                lines += 1
+
+        print("Bitmap ({}x{} pixels) converted to {} lines array".format(w,h,lines))
+        return s
+
+    except:
+        print("ERROR, could not convert pixels to string")
+        print(sys.exc_info())
+        return ""
+
+
 
 if __name__== "__main__":
-#    print("argc={} argv={}".format(len(sys.argv), sys.argv))
-    if len(sys.argv) == 1:
-        print("Usage: ./glcd_convert {[image1] [image2] ...}")
+    # print("argc={} argv={}".format(len(sys.argv), sys.argv))
+    if len(sys.argv) == 1 or "-h" in sys.argv or "--help" in sys.argv:
+        print("Usage: ./glcd_convert [-bpp 1|4] {[image1] [image2] ...}")
     else:
-        for infile in sys.argv[1:]:
-#            image_info(infile)
-            image_convert(infile)
+        bpp = 4
+        idx = 1
+        if "--bpp" in sys.argv:
+            idx = sys.argv.index('--bpp') + 1
+            bpp = int(sys.argv[idx])
+            #print("Bpp=%d" % bpp)
+            idx = idx + 1
+        if len(sys.argv) <= idx:
+            print("Usage: ./glcd_convert [-bpp 1|4] {[image1] [image2] ...}")
+        else:
+            for infile in sys.argv[idx:]:
+                image_info(infile)
+                image_convert(infile, bpp)
